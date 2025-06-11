cc117d51617d4515e04ab1830fde80c471fbea44
jerrypan102811 <150407185+jerrypan102811@users.noreply.github.com>
2025-06-11 16:23:00 +0800

    Update security.php

--- config/security.php
+++ config/security.php
@@ -9,6 +9,7 @@ ini_set('session.cookie_httponly', 1);
 ini_set('session.cookie_secure', 0); // Set to 1 for HTTPS
 ini_set('session.use_strict_mode', 1);
 
+session_start();
 /**
  * Security Helper Class
  */
